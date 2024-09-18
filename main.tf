31a86dedb6e1499761d8a7c9f50cfdfe218de639
Rodrigo Perisatto <99617955+perisatto@users.noreply.github.com>
2024-09-18 16:27:27 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,14 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+variable "db_user" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -37,8 +45,8 @@ resource "aws_db_instance" "menuguru" {
   allocated_storage    =  20
   engine_version       = "8.0"
   instance_class       = "db.t4g.micro"
-  username             = "root"
-  password             = "JLy5EYbobwCTvIuYFOcx"
+  username             = "${var.db_user}"
+  password             = "${var.db_password}"
   parameter_group_name = "default.mysql8.0"
   vpc_security_group_ids = ["${aws_security_group.rds_sg.id}"]
   skip_final_snapshot  = true
