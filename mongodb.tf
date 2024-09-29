d32c948caaa292a446e36c895883eb77342980c8
Rodrigo Perisatto <rodrigo@perisatto.com>
2024-09-29 10:46:31 -0300

    Minor fix

--- mongodb.tf
+++ mongodb.tf
@@ -61,22 +61,11 @@ resource "mongodbatlas_database_user" "db-user" {
 
 
 resource "mongodbatlas_cluster" "test" {
- project_id   = mongodbatlas_project.atlas-project.id
- name         = "${var.atlas_project_name}-${var.environment}-cluster"
- cluster_type = "REPLICASET"
- replication_specs {
-   num_shards = 1
-   regions_config {
-     region_name     = var.atlas_region
-     electable_nodes = 3
-     priority        = 7
-     read_only_nodes = 0
-   }
- }
- cloud_backup     = true
- auto_scaling_disk_gb_enabled = true
- mongo_db_major_version       = "7.0"
- # Provider Settings "block"
- provider_name               = var.cloud_provider
+ project_id                  = mongodbatlas_project.atlas-project.id
+ name                        = "${var.atlas_project_name}-${var.environment}-cluster"
+ cluster_type                = "REPLICASET"
+ provider_name               = "TENANT"
+ backing_provider_name       = var.cloud_provider
+ provider_region_name        = var.atlas_region
  provider_instance_size_name = var.cluster_instance_size_name
 }
